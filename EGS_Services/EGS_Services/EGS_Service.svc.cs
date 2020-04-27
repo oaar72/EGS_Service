9603894a9a40a0ccc5d334f79471ac15bced72d6
Oscar <romerooscar721>
2020-04-27 18:04:20 -0500

    Primer commit

---
+++ EGS_Services/EGS_Services/EGS_Service.svc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EGS_Services
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : EGS_Services
+    {
+        public string register(string name, string firstName, string lastName, DateTime date, string mail, string pass, string tel)
+        {
+            string token = "";
+
+            Network.Connection conn = new Network.Connection();
+            string conexion = conn.getConnectionString();
+
+            SqlConnection con = new SqlConnection(conexion);
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("register", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("@name", name));
+            cmd.Parameters.Add(new SqlParameter("@firstName", firstName));
+            cmd.Parameters.Add(new SqlParameter("@lastName", lastName));
+            cmd.Parameters.Add(new SqlParameter("@date", date));
+            cmd.Parameters.Add(new SqlParameter("@mail", mail));
+            cmd.Parameters.Add(new SqlParameter("@pass", pass));
+            cmd.Parameters.Add(new SqlParameter("@tel", tel));
+            cmd.Parameters.Add(new SqlParameter("@token", ""));
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            try
+            {
+                if (dr.Read())
+                {
+                    if (dr["ERROR"].ToString() == "")
+                    {
+                        token = dr["token"].ToString();
+                    }
+                    else
+                    {
+                        token = dr["ERROR"].ToString();
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                token += " Error al invocar SP (addUser). " + e.StackTrace;
+            }
+
+            return token;
+        }
+    }
+}
---
+++ EGS_Services/EGS_Services/Network/Connection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace EGS_Services.Network
+{
+    public class Connection
+    {
+        private string connectionString;
+        private string server;
+        private string username;
+        private string password;
+        private string DB;
+
+        public Connection()
+        {
+            server = ConfigurationManager.AppSettings["ServerDB"].ToString();
+            username = ConfigurationManager.AppSettings["UserDB"].ToString();
+            password = ConfigurationManager.AppSettings["PassDB"].ToString();
+            DB = ConfigurationManager.AppSettings["DB"].ToString();
+
+            //connectionString = "Server=tcp:" + server + ",1433;Initial Catalog=EGS;Persist Security Info=False; " + "User ID = " + username + ";Password=" + password + ";MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            connectionString = "data source=" + server + ";initial catalog=" + DB + ";user id= " + username + ";password="+ password + ";MultipleActiveResultSets=True;App=EntityFramework";
+        }
+
+        public string getConnectionString()
+        {
+            return this.connectionString;
+        }
+
+        public string getUsername()
+        {
+            return this.username;
+        }
+
+        public string getPassword()
+        {
+            return this.password;
+        }
+    }
+}
